e672aff00bf09ec6772d643aa659c6a8c2af7aa4
Sahu <amit-kumar.sahu@capgemini.com>
2025-04-19 21:24:50 +0530

    Added multiple methods for dao operations for user model

--- src/main/java/com/myjdbc/myjdbcapp/MyjdbcappApplication.java
+++ src/main/java/com/myjdbc/myjdbcapp/MyjdbcappApplication.java
@@ -1,12 +1,16 @@
 package com.myjdbc.myjdbcapp;
 
 import com.myjdbc.myjdbcapp.dao.UserDao;
+import com.myjdbc.myjdbcapp.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class MyjdbcappApplication implements CommandLineRunner {
@@ -22,8 +26,40 @@ public class MyjdbcappApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		logger.info("Num of rows affected - " + userDao.addUser(1, "Amit Sahu", "sahu.amit85@gmail.com"));
-		logger.info("All users - {}", userDao.getAllUsers());
-		logger.info("User by id 3 - {}", userDao.getUserById(3));
+		//		logger.info("Num of rows affected - " + userDao.addUser(1, "Amit Sahu", "sahu.amit85@gmail.com"));
+//		logger.info("Num of rows affected - " + userDao.addUser(2, "Ankit Gautam", "gautamankit99@gmail.com"));
+//		logger.info("Num of rows affected - " + userDao.addUser(3, "Ankit Sahoo", "sahoo.ankit85@gmail.com"));
+//		logger.info("Num of rows affected - " + userDao.addUser(4, "Sheik Sahil", "sahilsheik99@gmail.com"));
+//		logger.info("Num of rows affected - " + userDao.addUser(5, "Vikramaditya Swain", "swainvikramaditya89@gmail.com"));
+//		logger.info("All users - {}", userDao.getAllUsers());
+//		logger.info("User by id 1 - {}", userDao.getUserById(1));
+//		logger.info("updating user mail -> {}", userDao.updateUserEmailById(1, "amit-kumar.sahu@capgemini1.com"));
+//		logger.info("username by email -> {}", userDao.findUsernameByEmail("sahoo.ankit85@gmail.com"));
+//		logger.info("Deleting user with id {} is success {}", 3, userDao.deleteUserWithId(3));\
+//		List<User> users = Arrays.asList(
+//				new User(1, "Amit", "amitsahu@gmail.com"),
+//				new User(2, "Ankit", "ankit@gmail.com"),
+//				new User(3, "Sahil", "sahil@gmail.com"),
+//				new User(4, "Shubham", "shubham@yahoomail.com"),
+//				new User(5, "Swarna", "swarna@gmail.com"),
+//				new User(6, "Vikram", "vikram@gmail.com"),
+//				new User(7, "Abhishek", "abhishek@gmail.com"),
+//				new User(8, "Sampu", "sampu@gmail.com"),
+//				new User(9, "Suru", "suru@gmail.com"),
+//				new User(10, "Anant", "anant@gmail.com"),
+//				new User(11, "Aniket", "aniket@gmail.com"),
+//				new User(12, "Shivani", "shivani@gmail.com"),
+//				new User(13, "Priyanka", "priyankanahak@gmail.com"));
+//		logger.info("Doing bulk update and adding list of users - > {}", userDao.addBulkUsers(users));
+//		String domain = "gmail.com";
+//		logger.info("Users with domain - {} are  -> {}",domain, userDao.getUsersByEmailDomain(domain));
+//		String mail = "shivani@gmail.com";
+//		logger.info("User with email - {} exists - {}", mail, userDao.emailExists(mail));
+//		logger.info("Users deleted - {}", userDao.deleteAllUsers());
+//		logger.info("paged users data - {}", userDao.getUsersPaged(10, 5));
+//		String email  = "priyankanahak@gmail.com";
+//		logger.info("Get user by email - {} - {}", email, userDao.getUserByEmail(email));
+		User user = new User(20, "UniqueUser", "UniqueUser@gmail.com");
+		logger.info("Adding user if not exists already! - {} - status - {}", user.getEmail(), userDao.addUserIfNotExists(user));
 	}
 }
--- src/main/java/com/myjdbc/myjdbcapp/dao/UserDao.java
+++ src/main/java/com/myjdbc/myjdbcapp/dao/UserDao.java
@@ -1,7 +1,10 @@
 package com.myjdbc.myjdbcapp.dao;
 
 import com.myjdbc.myjdbcapp.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -13,6 +16,7 @@ import java.util.List;
 @Repository
 public class UserDao {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     JdbcTemplate template;
 
@@ -27,9 +31,151 @@ public class UserDao {
         return template.query(sql, new BeanPropertyRowMapper<User>(User.class));
     }
 
-    public User getUserById(int id){
-        String sql = "select * from user where id = ?";
-        List<User> resUsers = template.query(sql, new Object[]{id}, new BeanPropertyRowMapper<User>(User.class));
-        return resUsers.get(0);
+    public User getUserById(int id) {
+//        String sql = "SELECT * FROM users WHERE id = ?";
+        String sql = "select * from users where id = ?";
+        try{
+            return template.queryForObject(sql, new BeanPropertyRowMapper<User>(User.class), id);
+        }
+        catch (EmptyResultDataAccessException e){
+            logger.info("No user found with id - {}", id);
+            return null;
+        }
+    }
+
+    public int updateUserEmailById(int id, String newMail){
+        String sql = "update users set email = ? where id = ?";
+        try{
+            int updated = template.update(sql, newMail, id);
+            return updated;
+        }
+        catch (EmptyResultDataAccessException e)
+        {
+            logger.info("Something went wrong while updating! - " + e.getMessage());
+        }
+        return -1;
+    }
+
+    public String findUsernameByEmail(String email){
+        String sql = "select * from users where email = ?";
+        try{
+            User user = template.queryForObject(sql, new BeanPropertyRowMapper<User>(User.class), email);
+            return user.getName();
+        }
+        catch (EmptyResultDataAccessException e){
+            logger.info("No user with email - " + email + " found!");
+            return null;
+        }
+    }
+
+    public int deleteUserWithId(int id){
+        String sql = "delete from users where id = ?";
+        try{
+            int updated = template.update(sql, id);
+            return updated;
+        }
+        catch (EmptyResultDataAccessException e)
+        {
+            logger.info("Something went wrong while deleting user with id {}", id);
+
+        }
+        return -1;
+    }
+
+    public int [][] addBulkUsers(List<User> users){
+        String sql = "insert into users (id, name, email) values (?, ?, ?)";
+        try{
+            int[][] ints = template.batchUpdate(sql, users, users.size(), (ps, user) -> {
+                ps.setInt(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getEmail());
+            });
+            return ints;
+        }
+        catch (Exception e)
+        {
+            logger.info("Error while doing bulk addition of users - {}", e.getMessage());
+        }
+        return new int[][]{};
+    }
+
+    public List<User> getUsersByEmailDomain(String domain){
+        String sql = "select * from users where email like ?";
+        domain = "%"+domain;
+        try{
+            List<User> users = template.query(sql, new BeanPropertyRowMapper<>(User.class), domain);
+            return users;
+        }
+        catch (EmptyResultDataAccessException e){
+            logger.info(e.getMessage());
+        }
+        return null;
+    }
+
+    public boolean emailExists(String email){
+        String sql = "select count(*) from users where email = ?";
+        try{
+            Integer i = template.queryForObject(sql, Integer.class, email);
+            return i >= 1;
+        }
+        catch (EmptyResultDataAccessException e){
+            logger.info("empty result..");
+        }
+        return false;
+    }
+
+    public int deleteAllUsers(){
+        String sql = "delete from users";
+        try{
+            int updated = template.update(sql);
+            return updated;
+        }
+        catch (EmptyResultDataAccessException e){
+            logger.info(e.getMessage());
+            return -1;
+        }
+    }
+
+    public List<User> getUsersPaged(int offset, int limit){
+        String sql = "select * from users order by id limit ? offset ?";
+        try{
+            List<User> users = template.query(sql, new BeanPropertyRowMapper<>(User.class), limit, offset);
+            return users;
+        }
+        catch (EmptyResultDataAccessException e)
+        {
+            logger.info("Unable to fetch users - {}", e.getMessage());
+        }
+        return null;
+    }
+
+    public User getUserByEmail(String email){
+        String sql = "select * from users where email = ?";
+        try{
+            User user = template.queryForObject(sql, new BeanPropertyRowMapper<User>(User.class), email);
+            return user;
+        }
+        catch (Exception e){
+            logger.info(e.getMessage());
+            return null;
+        }
+    }
+
+    public boolean addUserIfNotExists(User user){
+        String sql = "insert into users (id, name, email) values (?, ?,  ?)";
+        if(!this.emailExists(user.getEmail())){
+            try{
+                int updated = template.update(sql, user.getId(), user.getName(), user.getEmail());
+                return true;
+            }
+            catch (Exception e){
+                logger.error("Error while inserting record with email - {}", user.getEmail());
+                return false;
+            }
+        }
+        else{
+            logger.info("User with email {} already exists!", user.getEmail());
+            return false;
+        }
     }
 }
